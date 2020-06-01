762cb3bca84e358a0dce2ec9624894e835705895
Ryan Duff <ryanduff613@gmail.com>
2020-06-01 14:39:55 -0700

    create users_controller.rb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Sign up succesful"
+      session[:user_id] = @user.user_id
+      redirect_to "/"
+    else  
+      flash[:alert] = "Errors encountered when signing up"
+      redirect_to '/signup'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :username, :password_confirmation)
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,15 @@ Class User < ApplicationRecord
   attr_accessor :password
   validates_confirmation_of :password
   validates :email, :presence => true, :uniqueness => true
+  validates :username, :presence => true
   before_save :encrypt_password
 
   def encrypt_password
     self.password_salt = BCrypt::Engine.generate_salt
     self.password_hash = BCrypt::Engine.hash_secret(password,password_salt)
-  encrypt_password
+  end
 
-  def self.authenticate(email, password)
+  def self.authenticate(email, password, username)
     user = User.find_by "email = ?", email
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
       user
